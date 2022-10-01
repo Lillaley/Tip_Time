c08d296c74ac82ca983ae9e4b0f5b2a0838abb36
Lillaley <malikatoken05@gmail.com>
2022-10-01 14:31:03 +0600

    adding calculatetip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -16,7 +16,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calculateTip() {
-
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
     }
-}
 }
